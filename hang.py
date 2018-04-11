38ff69020886eb0053bb53ec5e7afd11af4a2c0b
zero101010 <igotaraujo97@gmail.com>
2018-04-11 15:16:07 -0300

    change the argument of letter_guessed_again and others smalls changes in the structures

--- hang.py
+++ hang.py
@@ -1,35 +1,29 @@
 import random
 import string
-from docutils.writers.odf_odt import WORD_SPLIT_PAT1
+
 
 #load the file words in 
 def loadWords():
+   
     WORDLIST_FILENAME = "palavras.txt" #put the wordlist inside the method 
     print( "Loading word list from file...")
-    inFile = open(WORDLIST_FILENAME, 'r')  #perguntar mudança
+    inFile = open(WORDLIST_FILENAME, 'r')  #question to teacher
     line = inFile.readline()
     wordlist = str.split(line)#ver se n se pode mudar line
     print (len(wordlist), "words loaded.")
     random_word=random.choice(wordlist)
-    #print(random_word)
     random_word=change_word(random_word,wordlist)
-
-    '''
-    available = string.ascii_lowercase
-    while count_different_letters(available,random_word)>8:
-        random_word=random.choice(wordlist)
-    '''
+   
     return random_word
-
+#change the word if the word has more than 8 different letters
 def change_word(random_word,wordlist):
     available = string.ascii_lowercase
     while count_different_letters(available,random_word)>8:
         random_word=random.choice(wordlist)
-        #print ('changed')
     else:
         return random_word
-          
-def verify_letter_word(secretWord,lettersGuessed,guessed):
+#check if the letters was guessed           
+def check_letter_word(secretWord,lettersGuessed,guessed):
 
     for letter in secretWord:
         if letter in lettersGuessed:
@@ -37,15 +31,15 @@ def verify_letter_word(secretWord,lettersGuessed,guessed):
         else:
             guessed += '_ '
     return guessed
-
+#count how many different letters has in a word
 def count_different_letters(available,secretWord):
     for letter in available:
         if letter in secretWord:
             available = available.replace(letter, '')
             
     different_words=26-len(available)
-    #print ('This word have ',different_words,'different letters')
     return different_words
+#count how many letters has in a word
 def count_letters(available,lettersGuessed):
   
     for letter in available:
@@ -53,7 +47,7 @@ def count_letters(available,lettersGuessed):
             available = available.replace(letter, '')
         
     print ('Available letters', available)
-
+#
 def isWordGuessed(secretWord, lettersGuessed):
   
     for letter in secretWord:
@@ -63,29 +57,17 @@ def isWordGuessed(secretWord, lettersGuessed):
             return False
 
     return True
+#letter guessed more than
+def letter_guessed_again(guessed,lettersGuessed):
 
-def letter_guessed_again(letter,guessed,lettersGuessed):
-    '''
-    for letter in secretWord:
-        if letter in lettersGuessed:
-            guessed += letter
-        else:
-            guessed += '_ '
-        '''
-    guessed=verify_letter_word(secretWord,lettersGuessed,guessed)
+    guessed=check_letter_word(secretWord,lettersGuessed,guessed)
     print('Oops! You have already guessed that letter: ', guessed)
    
 def letter_good_guessed(letter,lettersGuessed):
     lettersGuessed.append(letter)   
     guessed = ''
-    '''
-    for letter in secretWord:
-        if letter in lettersGuessed:
-            guessed += letter
-        else:
-            guessed += '_ '
-    '''
-    guessed=verify_letter_word(secretWord,lettersGuessed,guessed)
+    
+    guessed=check_letter_word(secretWord,lettersGuessed,guessed)
     print( 'Good Guess: ', guessed)
 
 def letter_wrong_guessed(letter,lettersGuessed):
@@ -93,14 +75,8 @@ def letter_wrong_guessed(letter,lettersGuessed):
     lettersGuessed.append(letter)
    
     guessed = ''
-    '''
-    for letter in secretWord:
-        if letter in lettersGuessed:
-            guessed += letter
-        else:
-            guessed += '_ '
-    '''
-    guessed=verify_letter_word(secretWord,lettersGuessed,guessed)
+   
+    guessed=check_letter_word(secretWord,lettersGuessed,guessed)
     print ('Oops! That letter is not in my word: ',  guessed)
     
 
@@ -111,24 +87,22 @@ def hangman(secretWord):
     
     print( 'Welcome to the game, Hangam!')
     print('I am thinking of a word that is', len(secretWord), ' letters long.')
-    different_words = count_different_letters(available,secretWord)
-    print ('This word have ',different_words,'different letters')
     print ('-------------')
     
     
     while  isWordGuessed(secretWord, lettersGuessed) == False and guesses >0:#tentar arrumar com outra variavel ao invés de passar o método
         print ('You have ', guesses, 'guesses left.')
 
-       
-        
         count_letters(available,lettersGuessed)
 
         letter = input('Please guess a letter: ')
+
         if letter in lettersGuessed:
             guessed = ''
-            letter_guessed_again(letter,guessed,lettersGuessed)
-            
-          
+            letter_guessed_again(guessed,lettersGuessed)
+        elif letter =="tip":
+            different_words = count_different_letters(available,secretWord)
+            print ('This word have ',different_words,'different letters')
         elif letter in secretWord:
             letter_good_guessed(letter,lettersGuessed)
           
