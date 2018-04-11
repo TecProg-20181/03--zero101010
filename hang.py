d8cfd526fd6812e32edef1be6bf2734591b8bb6e
zero101010 <igotaraujo97@gmail.com>
2018-04-11 10:55:33 -0300

    add method  count_diferent_letters

--- hang.py
+++ hang.py
@@ -3,15 +3,22 @@ import string
 
 #load the file words in 
 def loadWords():
-    WORDLIST_FILENAME = "palavras.txt" #put the wordlist inside the method 
+    WORDLIST_FILENAME = "words.txt" #put the wordlist inside the method 
     print( "Loading word list from file...")
     inFile = open(WORDLIST_FILENAME, 'r')  #perguntar mudança
     line = inFile.readline()
     wordlist = str.split(line)#ver se n se pode mudar line
     print ("  ", len(wordlist), "words loaded.")
     return random.choice(wordlist)
-def diferents_letters(available,lettersGuessed):
-    #available = string.ascii_lowercase
+
+def count_diferent_letters(available,secretWord):
+    for letter in available:
+        if letter in secretWord:
+            available = available.replace(letter, '')
+    print ('This word have ',26-len(available),'diferent letters')
+
+def count_letters(available,lettersGuessed):
+  
     for letter in available:
         if letter in lettersGuessed:
             available = available.replace(letter, '')
@@ -20,7 +27,6 @@ def diferents_letters(available,lettersGuessed):
 
 def isWordGuessed(secretWord, lettersGuessed):
   
-    
     for letter in secretWord:
         if letter in lettersGuessed:
             pass
@@ -30,8 +36,7 @@ def isWordGuessed(secretWord, lettersGuessed):
     return True
 
 def letter_guessed_again(letter,guessed,lettersGuessed):
-    #lettersGuessed = []
-   
+
     for letter in secretWord:
         if letter in lettersGuessed:
             guessed += letter
@@ -65,25 +70,23 @@ def letter_wrong_guessed(letter,lettersGuessed):
     
 
 def hangman(secretWord):
-    #available = string.ascii_lowercase
+    available = string.ascii_lowercase
     guesses = 8
     lettersGuessed = []
+    
     print( 'Welcome to the game, Hangam!')
     print('I am thinking of a word that is', len(secretWord), ' letters long.')
+    count_diferent_letters(available,secretWord)
     print ('-------------')
-
+    
+    
     while  isWordGuessed(secretWord, lettersGuessed) == False and guesses >0:#tentar arrumar com outra variavel ao invés de passar o método
         print ('You have ', guesses, 'guesses left.')
 
-        available = string.ascii_lowercase
-        '''
-        for letter in available:
-            if letter in lettersGuessed:
-                available = available.replace(letter, '')
+       
         
-        print ('Available letters', available)
-'''     
-        diferents_letters(available,lettersGuessed)
+        count_letters(available,lettersGuessed)
+
         letter = input('Please guess a letter: ')
         if letter in lettersGuessed:
             guessed = ''
