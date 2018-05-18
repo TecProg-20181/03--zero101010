19ea5ed034fc64d6e12462d2ac7ed1ebb7cbe5dc
zero101010 <igotaraujo97@gmail.com>
2018-05-18 05:13:55 -0300

    add asserts and create the methods to see if is None or if have the specific type

--- hang.py
+++ hang.py
@@ -1,18 +1,33 @@
 import random
 import string
 
+def error_params_none(param,msg):
+    assert param !=None,msg 
+def error_params_type(param,type_param,msg):
+    assert type(param)!=type_param,msg 
+
 
 class Words(object):
 
     def word_different_letters(self, available, secretWord):
+        
+        error_params_none(available,"available recebe nulo")
+        error_params_type(available,string,"available não é String")
+        error_params_none(secretWord,"secretWord recebe nulo")
+        error_params_type(secretWord,string,"secretWord não é String")
 
         for letter in available:
             if letter in secretWord:
                 available = available.replace(letter, '')
-        different_words = 26-len(available)
+        different_words = 26-len(available)     
         return different_words
 
     def change_word(self, random_word, word_list):
+        
+        error_params_none(random_word,"random_word está recebendo nulo")
+        error_params_type(random_word,string,"random_word não é String")
+        error_params_none(word_list,"word_list recebe nulo")
+        error_params_type(word_list,string,"word_list não é String")
 
         available = string.ascii_lowercase
         while self.word_different_letters(available, random_word) > 8:
@@ -20,8 +35,8 @@ class Words(object):
         else:
             return random_word
 
-    # load the file words in
-    def loadWords(self):
+    
+    def load_words(self):
 
         list_of_words = "palavras.txt"
         #print("Loading word list from file...")
@@ -34,6 +49,11 @@ class Words(object):
         return random_word
 
     def isWordGuessed(self, secretWord, lettersGuessed):
+        
+        error_params_none(secretWord,"secretWord está recebendo nulo")
+        error_params_type(secretWord,string,"secretWord não é String")
+        error_params_none(lettersGuessed,"lettersGuessed recebe nulo")
+        error_params_type(lettersGuessed,string,"lettersGuessed não é String")        
 
         for letter in secretWord:
             if letter in lettersGuessed:
@@ -47,6 +67,13 @@ class Letters(object):
 
     def check_letter_word(self, secretWord, lettersGuessed, guessed):
 
+        error_params_none(secretWord,"secretWord está recebendo nulo")
+        error_params_type(secretWord,string,"secretWord não é String")
+        error_params_none(lettersGuessed,"lettersGuessed recebe nulo")
+        error_params_type(lettersGuessed,string,"lettersGuessed não é String")  
+        error_params_none(guessed,"guessed recebe nulo")
+        error_params_type(guessed,string,"guessed não é String") 
+
         for letter in secretWord:
             if letter in lettersGuessed:
                 guessed += letter
@@ -56,6 +83,12 @@ class Letters(object):
 
     def count_letters(self, available, lettersGuessed):
 
+        error_params_none(available,"available está recebendo nulo")
+        error_params_type(available,string,"available não é String")
+        error_params_none(lettersGuessed,"lettersGuessed recebe nulo")
+        error_params_type(lettersGuessed,string,"lettersGuessed não é String")  
+
+
         for letter in available:
             if letter in lettersGuessed:
                 available = available.replace(letter, '')
@@ -64,10 +97,22 @@ class Letters(object):
 
     def letter_guessed_again(self, guessed, lettersGuessed):
 
+        error_params_none(lettersGuessed,"lettersGuessed recebe nulo")
+        error_params_type(lettersGuessed,string,"lettersGuessed não é String")  
+        error_params_none(guessed,"guessed recebe nulo")
+        error_params_type(guessed,string,"guessed não é String") 
+
+
         guessed = self.check_letter_word(secretWord, lettersGuessed, guessed)
         print('Oops! You have already guessed that letter: ', guessed)
 
     def letter_good_guessed(self, letter, lettersGuessed):
+
+        error_params_none(lettersGuessed,"lettersGuessed recebe nulo")
+        error_params_type(lettersGuessed,string,"lettersGuessed não é String")  
+        error_params_none(letter,"letter recebe nulo")
+        error_params_type(letter,string,"letter não é String") 
+
         
         lettersGuessed.append(letter)
         guessed = ''
@@ -76,6 +121,11 @@ class Letters(object):
 
     def letter_wrong_guessed(self, letter, lettersGuessed):
 
+        error_params_none(lettersGuessed,"lettersGuessed recebe nulo")
+        error_params_type(lettersGuessed,string,"lettersGuessed não é String")  
+        error_params_none(letter,"letter recebe nulo")
+        error_params_type(letter,string,"letter não é String") 
+
         lettersGuessed.append(letter)
         guessed = ''
         guessed = self.check_letter_word(secretWord, lettersGuessed, guessed)
@@ -83,12 +133,15 @@ class Letters(object):
 
 
 def hangman(secretWord):
+
+    error_params_none(secretWord,"secretWord está recebendo nulo")
+    error_params_type(secretWord,string,"secretWord não é String")
+    
     available = string.ascii_lowercase
     guesses = 8
     lettersGuessed = []
     letters = Letters()
-    words = Words()
-    alphabetic_letters = string.ascii_letters
+   # words = Words()
     jumpline='\n'
 
     print('Welcome to the game, Hangam!')
@@ -99,12 +152,11 @@ def hangman(secretWord):
 
     while words.isWordGuessed(secretWord, lettersGuessed) == False and guesses > 0:
         print('You have ', guesses, 'guesses left.')
-
         letters.count_letters(available, lettersGuessed)
 
         letter = input('Please guess a letter: ')
         letter= letter.lower()
-        if letter not in alphabetic_letters:
+        if letter not in string.ascii_letters:
             print("This is not a letter,try again with letters")
         
         elif letter in jumpline:
@@ -126,10 +178,11 @@ def hangman(secretWord):
     else:
         if words.isWordGuessed(secretWord, lettersGuessed) == True:
             print('Congratulations, you won!')
+        
         else:
             print('Sorry, you ran out of guesses. The word was ', secretWord, '.')
 
 
 words = Words()
-secretWord = words.loadWords().lower()
+secretWord = words.load_words().lower()
 hangman(secretWord)
