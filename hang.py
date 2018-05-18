788e430dac7ed906f772894f68481e4a6fd3a2e6
zero101010 <igotaraujo97@gmail.com>
2018-05-18 20:16:35 -0300

    add logging

--- hang.py
+++ hang.py
@@ -36,13 +36,13 @@ class Words(object):
 
     
     def load_words(self):
-        logging.basicConfig(filename = 'teste.log',level=logging.DEBUG)
+
         list_of_words = "palavras.txt"
-        logging.debug("Loading word list from file...")
+        #print("Loading word list from file...")
         load_file = open(list_of_words, 'r')
         line = load_file.readline()
         word_list = str.split(line)
-        logging.debug(len(word_list), "words loaded.")
+        print(len(word_list), "words loaded.")
         choice_word = random.choice(word_list)
         random_word = self.change_word(choice_word, word_list)
         return random_word
@@ -118,20 +118,25 @@ class Letters(object):
 
 def hangman(secretWord):
 
+    logging.basicConfig(filename='filelog.log',level=logging.DEBUG)
     apply_asserts(secretWord,string,"secretWord recebe nulo","secretWord não é String")
     
     available = string.ascii_lowercase
     guesses = 8
     lettersGuessed = []
     letters = Letters()
-   # words = Words()
     jumpline='\n'
-
+    
     print('Welcome to the game, Hangam!')
     print('I am thinking of a word that is', len(secretWord), ' letters long.')
+
+    logging.debug("start")
+    
     different_words = words.word_different_letters(available, secretWord)
     print('This word have ', different_words, 'different letters')
     print('-------------')
+
+    logging.debug("read the quantity of words in the file")
 
     while words.isWordGuessed(secretWord, lettersGuessed) == False and guesses > 0:
         print('You have ', guesses, 'guesses left.')
@@ -139,33 +144,36 @@ def hangman(secretWord):
 
         letter = input('Please guess a letter: ')
         letter= letter.lower()
+        logging.debug("Enter with the letter ")
+        
         if letter not in string.ascii_letters:
             print("This is not a letter,try again with letters")
+            logging.debug("Enter with something that ins't a letter ")
         
         elif letter in jumpline:
             print("write something")
-
+            logging.debug("The letter is Null")
         elif letter in lettersGuessed:
             guessed = ''
             letters.letter_guessed_again(guessed, lettersGuessed)
-        
+            logging.debug("The letter was written")
         elif letter in secretWord and letter != None:
             letters.letter_good_guessed(letter, lettersGuessed)
-
+            logging.debug("The letter is in the word")
         else:
             guesses -= 1
             letters.letter_wrong_guessed(letter, lettersGuessed)
-
+            logging.debug("The letter isn't in the word")
         print('------------')
 
     else:
         if words.isWordGuessed(secretWord, lettersGuessed) == True:
             print('Congratulations, you won!')
-        
+            logging.debug("The player won")
         else:
             print('Sorry, you ran out of guesses. The word was ', secretWord, '.')
-
-
+            logging.debug("The player lose ")
+    logging.debug("finish the program")
 words = Words()
 secretWord = words.load_words().lower()
 hangman(secretWord)
